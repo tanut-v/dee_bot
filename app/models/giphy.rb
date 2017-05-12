4af63de57fd4f2a7f113344036a65ae82b85c15f
Tanut Viteevanich <tanut.viteevanich@rockyou.com>
2017-05-12 14:30:15 +0700

    Replace space with '+'.

--- app/models/giphy.rb
+++ app/models/giphy.rb
@@ -3,7 +3,7 @@ require 'net/http'
 class Giphy
 
   def self.search(query)
-    url = URI.parse("http://api.giphy.com/v1/gifs/search?q=#{query}&api_key=dc6zaTOxFJmzC")
+    url = URI.parse("http://api.giphy.com/v1/gifs/search?q=#{query.tr(' ', '+')}&api_key=dc6zaTOxFJmzC")
     request = Net::HTTP::Get.new(url.to_s)
     response = Net::HTTP.start(url.host, url.port) do |http|
       http.request(request)
