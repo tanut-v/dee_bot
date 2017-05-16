5dc1915bd2c4bf754e05b4a0e7e8aad286b1dfcd
Tanut Viteevanich <tanut.viteevanich@rockyou.com>
2017-05-16 17:17:02 +0700

    Encode utf-8.

--- app/models/giphy.rb
+++ app/models/giphy.rb
@@ -4,6 +4,7 @@ class Giphy
 
   def self.search(query)
     offset = rand(1..25)
+    query = query.encode("UTF-8")
 
     url = URI.parse("http://api.giphy.com/v1/gifs/search?q=#{query.tr(' ', '+')}&api_key=dc6zaTOxFJmzC&limit=25&offset=#{offset}&lang=th")
     request = Net::HTTP::Get.new(url.to_s)
