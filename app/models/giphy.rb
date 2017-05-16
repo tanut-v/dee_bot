60d6539eab25883317cfb9ac7e833ca85020a357
Tanut Viteevanich <tanut.viteevanich@rockyou.com>
2017-05-16 17:20:38 +0700

    Remove TH.

--- app/models/giphy.rb
+++ app/models/giphy.rb
@@ -4,9 +4,8 @@ class Giphy
 
   def self.search(query)
     offset = rand(1..25)
-    query = query.encode("UTF-8")
 
-    url = URI.parse("http://api.giphy.com/v1/gifs/search?q=#{query.tr(' ', '+')}&api_key=dc6zaTOxFJmzC&limit=25&offset=#{offset}&lang=th")
+    url = URI.parse("http://api.giphy.com/v1/gifs/search?q=#{query.tr(' ', '+')}&api_key=dc6zaTOxFJmzC&limit=25&offset=#{offset}")
     request = Net::HTTP::Get.new(url.to_s)
     response = Net::HTTP.start(url.host, url.port) do |http|
       http.request(request)
